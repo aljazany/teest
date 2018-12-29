1b17cae3ec38960992a9a709466be9bea1252522
abdullah alsalem <aaljazany@gmail.com>
2018-12-29 19:29:53 +0300

    Initial Commit

---
+++ Alhsn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alhsn
+//
+//  Created by ABDULLAH AL SALAM on 22/04/1440 AH.
+//  Copyright © 1440 ABDULLAH AL SALAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
